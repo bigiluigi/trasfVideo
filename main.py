c2ae9ef510abe6bf44e629c733b891538e70e718
bigiluigi <129006473+bigiluigi@users.noreply.github.com>
2025-03-22 22:53:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import requests
 import time
 
 # Configurazione
-START_ID = 840865  # ID iniziale
+START_ID = 841836  # ID iniziale
 END_ID = 849879    # ID finale 
 DELAY = 3          # Secondi tra le richieste
 COOKIES = {
     "xfsts": "06we6yy56eiou0yj",
     "login": "axelfire",
-    "cf_clearance": "e2aR84Q5J0g.CXjo9QYwacFEba500MnjcwnbDexA__M-1742679156-1.2.1.1-jAXzh7gX_XuGrkeCbZPUvxaByAlG4ysZ9bXg1GgenpoHngGdJPlhAaGGIhkUOsPWiDGLjLeyMjpfc7HZCXqI.O75qONiY.FvZkejNxPrZ2i2RqrXjYEP4PdlIgfQTDS3_06m3Pzonn8AqbV8GbEvj3yTf.qSsIAX5duj8DqmwpYdwcnFR6ZPMAj8_nRWE8kYQiY0EfnyPKcFQMMUWq1lGjvjgGRCCwlOM.lOpBHx9ZXor_hhKhGXEuWi4x7_TjQZECaeK1miYzILbA7HCC.NCbL3egFaIqg0zEb4xq_mMp2GMix0.DcyUZITgST9pVPefyWRZa5mH3W9sqkaGU68d8Zm3aCCYy_cAN7hQBO73Ac",
+    "cf_clearance": "Nme1eBDRAUEG3HaYXCCvZgxngi7bykJa2qqN.g6EMbY-1742680165-1.2.1.1-_SKFTUEgDkIjKWuHcYObOyBctEK_4P.irPURh0J1ybiyAoVN6yZAQ05RGdf.C8U1zdG0.byiK4umswsWVAPPhC4wpSOVPsWmJf7KeJOi5MPrfQg_EI59OujWBb_fmy6dSDTTPmNaup2FohnLCpHzXpjM.s0joOjtkpMuZCNWAKYFmQqI.0M7wWeH.Eg_GgGY6vM8zidLQwnMCHkZWckbqdnyLag_dmz0MJXqZjNm0aAvnyASBiWyIMHrYhWhA_dPiIpJOLijhH.fX1EWdzRedbtsVyILkPFjwfw18waWzlxHeeseriGm4y7myeXjWiH8mvHhd7hQFNK9MlfFhhdoVlkNq4b7aT6DRv4BNIexhkg",
     # Aggiungi altri cookie se necessario
 }
 
